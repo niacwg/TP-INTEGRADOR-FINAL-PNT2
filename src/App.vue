3216dfcac04347d2a6f113d0fb17ff9324241c4e
angelicav <angelicavel00@gmail.com>
2025-11-10 21:36:52 -0300

    Formulario checkout del pago del carrito

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <h1>Formulario de pago</h1>
+    <CheckoutView />
+  </div>
 </template>
+
+<script>
+import CheckoutView from './views/CheckoutView.vue'
+
+export default {
+  name: 'App',
+  components: { CheckoutView }
+}
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  padding: 2rem;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
 }
 </style>
